bf5088f3b45e96c48130705411e157fbc6558961
ismail <ismail.hossain@cloudwell.co>
2020-06-08 20:01:08 +0600

    Request Money

--- app/src/main/java/com/cloudwell/paywell/consumer/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/cloudwell/paywell/consumer/ui/account/AccountViewModel.kt
@@ -3,8 +3,8 @@ package com.cloudwell.paywell.consumer.ui.account
 import android.content.Intent
 import android.view.View
 import androidx.lifecycle.ViewModel
-import com.cloudwell.paywell.consumer.activity.registation.SignupActivity
 import com.cloudwell.paywell.consumer.ui.addMoney.AddMoneyHostActivity
+import com.cloudwell.paywell.consumer.ui.requestMoney.RequestMoneyHostActivity
 import com.cloudwell.paywell.consumer.ui.scheduledTransfer.SchedulTransferHostActivity
 import com.cloudwell.paywell.consumer.ui.sendMoney.SendMoneyHostActivity
 
@@ -23,7 +23,7 @@ class AccountViewModel : ViewModel() {
     }
 
     fun requestMoney(view: View) {
-        Intent(view.context, SignupActivity::class.java).also {
+        Intent(view.context, RequestMoneyHostActivity::class.java).also {
             view.context.startActivity(it)
         }
     }
