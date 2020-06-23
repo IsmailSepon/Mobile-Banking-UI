4f4839008fd116dc7a875028cbfd80aed50dbd8d
ismail <ismail.hossain@cloudwell.co>
2020-06-24 02:50:25 +0600

    beneficery back button done

--- app/src/main/java/com/cloudwell/paywell/ui/beneficiary/fragment/AddCardAccountFragment.kt
+++ app/src/main/java/com/cloudwell/paywell/ui/beneficiary/fragment/AddCardAccountFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cloudwell.paywell.R
 import com.cloudwell.paywell.ui.beneficiary.viewModel.BeneficeryViewModel
-import kotlinx.android.synthetic.main.addaccount_layout.view.*
+import com.cloudwell.paywell.utils.FragmentHelper
+import kotlinx.android.synthetic.main.addaccount_layout.view.address_btn
+import kotlinx.android.synthetic.main.addcardaccount_layout.view.*
 
 class AddCardAccountFragment : Fragment() {
 
@@ -38,6 +40,9 @@ class AddCardAccountFragment : Fragment() {
 //        binding.lifecycleOwner = this
 //        return binding.root
 
+        view.card_detils_back.setOnClickListener(View.OnClickListener {
+            FragmentHelper.removeFragment(activity?.supportFragmentManager)
+        })
 
         return view
     }
