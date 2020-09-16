dee41e658724e5013543bc797624f85ca5165950
ismail.hossain <ismail.hossain@cloudwell.co>
2020-09-16 13:47:40 +0600

    Invoice Print

---
+++ app/src/main/java/com/cloudwell/paywell/uiBusiness/cards/fragment/invoice/BuInvoicePrintFragment.kt
@@ -0,0 +1,20 @@
+package com.cloudwell.paywell.uiBusiness.cards.fragment.invoice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cloudwell.paywell.R
+
+class BuInvoicePrintFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View =
+            inflater.inflate(R.layout.invoice_print_fragment, container, false)
+        return view
+    }
+}
--- app/src/main/java/com/cloudwell/paywell/uiBusiness/cards/fragment/invoice/InvoiceMainFragment.kt
+++ app/src/main/java/com/cloudwell/paywell/uiBusiness/cards/fragment/invoice/InvoiceMainFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.cloudwell.paywell.R
 import com.cloudwell.paywell.uiBusiness.cards.adapter.InvoicePagerAdapter
+import com.cloudwell.paywell.utils.FragmentHelper
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.invoice_main_layout.view.*
 
 class InvoiceMainFragment : Fragment() {
 
@@ -33,6 +35,11 @@ class InvoiceMainFragment : Fragment() {
 
 
 
+        view.create_invoice.setOnClickListener(View.OnClickListener {
+            FragmentHelper.replaceFragment(
+                BuInvoicePrintFragment(), requireActivity().supportFragmentManager, R.id.bu_Cards_container
+            )
+        })
 
 
 
