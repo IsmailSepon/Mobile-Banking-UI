fdf1522ed67141065725ff24c0cae8f1b3cabf62
ismail.hossain <ismail.hossain@cloudwell.co>
2020-10-04 22:25:06 +0600

    Service UI2

---
+++ app/src/main/java/com/cloudwell/paywell/uiCommon/pay/fragment/invite/StartInvitingFragment.kt
@@ -0,0 +1,26 @@
+package com.cloudwell.paywell.uiCommon.pay.fragment.invite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cloudwell.paywell.R
+
+class StartInvitingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.start_inviting_layout, container, false)
+
+
+
+        return view
+    }
+
+
+}
