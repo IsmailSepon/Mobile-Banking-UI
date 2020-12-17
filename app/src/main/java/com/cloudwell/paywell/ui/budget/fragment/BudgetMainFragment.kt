2e8c97145936f5508f5f93899c1a4a344adfdf17
ismail <ismailhossainsepon@gmail.com>
2020-12-18 03:12:32 +0600

    Budget graph added

--- app/src/main/java/com/cloudwell/paywell/ui/budget/fragment/BudgetMainFragment.kt
+++ app/src/main/java/com/cloudwell/paywell/ui/budget/fragment/BudgetMainFragment.kt
@@ -2,6 +2,7 @@ package com.cloudwell.paywell.ui.budget.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.broooapps.graphview.CurveGraphConfig
+import com.broooapps.graphview.CurveGraphView
+import com.broooapps.graphview.models.GraphData
+import com.broooapps.graphview.models.PointMap
 import com.cloudwell.paywell.R
 import com.cloudwell.paywell.ui.addMoney.viewModel.AddMoneyViewModel
 import com.cloudwell.paywell.ui.budget.BudgetHostActivity
@@ -20,6 +25,7 @@ import com.cloudwell.paywell.ui.budget.layoutManager.BudgetLayoutManager
 import com.cloudwell.paywell.ui.budget.model.BudgetPOjo
 import com.example.nbtk.slider.ScreenUtils
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.budget_main_layout.*
 import kotlinx.android.synthetic.main.budget_main_layout.view.*
 
 class BudgetMainFragment : Fragment() {
@@ -29,10 +35,13 @@ class BudgetMainFragment : Fragment() {
     private lateinit var tvSelectedItem: TextView
     private lateinit var sliderAdapter: BudgetAdapter
 
-    var selection = arrayOf("All", "All", "All")
+    var selection = arrayOf("All", "Week", "Month")
 
     private val data: ArrayList<String> = ArrayList()
     private val slidedata: ArrayList<BudgetPOjo> = ArrayList()
+
+
+    var curveGraphView: CurveGraphView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,7 +52,11 @@ class BudgetMainFragment : Fragment() {
 
 
         view.selection_spinner.onItemSelectedListener
-        val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(requireContext(), R.layout.budget_spinner_tem, selection)
+        val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            requireContext(),
+            R.layout.budget_spinner_tem,
+            selection
+        )
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         view.selection_spinner.adapter = aa
 
@@ -75,9 +88,17 @@ class BudgetMainFragment : Fragment() {
         setHorizontalPicker(view)
 
 
+        curveGraphView = view.budget_graphview
+        setBudgetGraph()
 
 
-        val beneficiaryDetailsPagerAdapter = BudgetPagerAdapter(requireActivity().applicationContext,requireActivity().supportFragmentManager)
+
+
+
+        val beneficiaryDetailsPagerAdapter = BudgetPagerAdapter(
+            requireActivity().applicationContext,
+            requireActivity().supportFragmentManager
+        )
         val viewPager: ViewPager = view.findViewById(R.id.budget_view_pager)
         viewPager.adapter = beneficiaryDetailsPagerAdapter
         val tabs: TabLayout = view.findViewById(R.id.budget_tab)
@@ -97,7 +118,7 @@ class BudgetMainFragment : Fragment() {
 
         view.add_budget.setOnClickListener(View.OnClickListener {
 
-            val intent : Intent = Intent(requireContext(), BudgetHostActivity::class.java)
+            val intent: Intent = Intent(requireContext(), BudgetHostActivity::class.java)
             intent.putExtra("budget", 1)
             requireContext().startActivity(intent)
 
@@ -108,7 +129,7 @@ class BudgetMainFragment : Fragment() {
 
         view.imageView320.setOnClickListener(View.OnClickListener {
 
-            val intent : Intent = Intent(requireContext(), BudgetHostActivity::class.java)
+            val intent: Intent = Intent(requireContext(), BudgetHostActivity::class.java)
             intent.putExtra("budget", 2)
             requireContext().startActivity(intent)
 
@@ -122,12 +143,67 @@ class BudgetMainFragment : Fragment() {
         return view
     }
 
+    private fun setBudgetGraph() {
+
+
+        curveGraphView!!.configure(
+            CurveGraphConfig.Builder(requireContext())
+                .setAxisColor(R.color.keypad_text_clr)             // Set number of values to be displayed in X ax
+                .setVerticalGuideline(7)                // Set number of background guidelines to be shown.
+                //  .setHorizontalGuideline(2)
+                .setGuidelineColor(R.color.Red)         // Set color of the visible guidelines.
+                .setNoDataMsg(" No Data ")              // Message when no data is provided to the view.
+                .setxAxisScaleTextColor(R.color.Black)  // Set X axis scale text color.
+                .setyAxisScaleTextColor(R.color.Black) // Set Y axis scale text color
+                .setAnimationDuration(2000)             // Set Animation Duration
+                .build()
+        )
+
+
+        val pointMap = PointMap()
+        pointMap.addPoint(0, 200)
+        pointMap.addPoint(1, 200)
+        pointMap.addPoint(2, 200)
+        pointMap.addPoint(3, 400)
+        pointMap.addPoint(4, 100)
+        pointMap.addPoint(5, 600)
+
+
+        val gd = GraphData.builder(requireContext())
+            .setPointMap(pointMap)
+            .setGraphStroke(R.color.Black).setGraphGradient(R.color.keypad_text_clr, R.color.light_blue)
+            .animateLine(true)
+            .setPointColor(R.color.Red)
+            .setPointRadius(5)
+            .build()
+
+        val p2 = PointMap()
+        p2.addPoint(0, 440)
+        p2.addPoint(1, 0)
+        p2.addPoint(2, 100)
+        p2.addPoint(3, 0)
+        p2.addPoint(4, 400)
+        p2.addPoint(5, 200)
+
+        val gd2 = GraphData.builder(requireContext())
+            .setPointMap(p2)
+            .setGraphStroke(R.color.Green)
+            .setGraphGradient(R.color.gradientStartColor, R.color.gradientEndColor)
+            .animateLine(true)
+            .build()
+
+        Handler().postDelayed({ curveGraphView!!.setData(5, 1000, gd, gd2) }, 250)
+    }
+
 
     private fun setHorizontalPicker(view: View) {
         rvHorizontalPicker = view.findViewById(R.id.budget_picker)
 
         // Setting the padding such that the items will appear in the middle of the screen
-        val padding: Int = ScreenUtils.getScreenWidth(view.context) / 2  - ScreenUtils.dpToPx(view.context, 100)
+        val padding: Int = ScreenUtils.getScreenWidth(view.context) / 2  - ScreenUtils.dpToPx(
+            view.context,
+            100
+        )
 
         rvHorizontalPicker.setPadding(padding, 0, padding, 0)
         val layoutManager = LinearLayoutManager(requireContext())
