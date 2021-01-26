d385e4a3b0721e8d84f3c8a2fe740a4bfd0e1211
ismail <ismailhossainsepon@gmail.com>
2021-01-26 17:37:39 +0600

    Pre PSP version setup

---
+++ app/src/main/java/com/cloudwell/paywell/PrePSPVersion/PrePSPsplashActivity.kt
@@ -0,0 +1,39 @@
+package com.cloudwell.paywell.PrePSPVersion
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.cloudwell.paywell.R
+import com.cloudwell.paywell.base.ApplockManager
+
+class PrePSPsplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH = 3000
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pre_p_s_psplash)
+
+
+//        val preference : Preference = Preference.getInstance(this)
+//        val userMobileNumber: String = preference.getData("userMobileNumber")
+//        Log.e("userMobileNumber", userMobileNumber)
+
+        ApplockManager.getInstance().enableDefaultAppLockIfAvailable(application)
+
+        Handler().postDelayed({
+
+//            if (userMobileNumber!= ""){
+//                val mainIntent = Intent(this, UserAuthenticationHostActivity::class.java)
+//                this.startActivity(mainIntent)
+//            }else{
+//                val mainIntent = Intent(this, SignupActivity::class.java)
+//                this.startActivity(mainIntent)
+//            }
+
+
+          //  this.finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
---
+++ app/src/main/java/com/cloudwell/paywell/PrePSPVersion/Ui/registration_Login/PAuthenticationHostActivity.kt
@@ -0,0 +1,12 @@
+package com.cloudwell.paywell.PrePSPVersion.Ui.registration_Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cloudwell.paywell.R
+
+class PAuthenticationHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p_authentication_host)
+    }
+}
