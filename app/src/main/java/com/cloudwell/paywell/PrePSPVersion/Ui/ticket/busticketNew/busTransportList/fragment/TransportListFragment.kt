c5e26a1aeefb3f6c8d57270313aad68df6057a68
ismail <ismailhossainsepon@gmail.com>
2021-03-22 22:49:31 +0600

    done

--- app/src/main/java/com/cloudwell/paywell/PrePSPVersion/Ui/ticket/busticketNew/busTransportList/fragment/TransportListFragment.kt
+++ app/src/main/java/com/cloudwell/paywell/PrePSPVersion/Ui/ticket/busticketNew/busTransportList/fragment/TransportListFragment.kt
@@ -9,8 +9,11 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.PopupWindow
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -123,12 +126,18 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
     }
 
     private fun initilization(view: View) {
-        view.ivFilterUpDown.setImageResource(0)
-        view.ivFilterUpDown.setImageResource(R.drawable.arrow_down)
+//        view.ivFilterUpDown.setImageResource(0)
+//        view.ivFilterUpDown.setImageResource(R.drawable.arrow_down)
 
         view.search_bus_back.setOnClickListener(View.OnClickListener {
 
             requireActivity().finish()
+        })
+
+        view.llCustomFilterHeader.setOnClickListener(View.OnClickListener {
+
+            hiddenAndShowFilter(view)
+
         })
 
 
@@ -139,89 +148,91 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
         })
 
 
-        view.llCustomFilterHeader.setOnClickListener {
+//        view.llCustomFilterHeader2.setOnClickListener {
+//            hiddenAndShowFilter(view)
+//        }
+
+        view.ivUp.setOnClickListener {
             hiddenAndShowFilter(view)
         }
 
-//        view.ivUp.setOnClickListener {
-//            hiddenAndShowFilter(view)
-//        }
+        view.viewSearch.setOnClickListener {
+            hiddenAndShowFilter(view)
 
-//        view.viewSearch.setOnClickListener {
-//            hiddenAndShowFilter(view)
+            val m = SearchFilter()
+
+            val checkedRadioButtonId = view.radioGroupJounyTime.checkedRadioButtonId
+            val departingTime = when (checkedRadioButtonId) {
+                R.id.radioJourneyTimeAll -> "Any"
+                R.id.radioJourneyTimeAllMorning -> "Morning"
+                R.id.radioJourneyTimeAllEvening -> "Evening"
+              //  R.id.radioJourneyTimeAfterNoon -> "AfterNoon"
+                R.id.radioJourneyTimeAllNight -> "Night"
+
+                else -> "Any"
+            }
+            m.departingTime = departingTime
+
+
+            val id = view.radioGroupJounryType.checkedRadioButtonId
+            val coachType = when (id) {
+                R.id.radioBtmAll -> "All"
+                R.id.radioBtmAC -> "Ac"
+                R.id.radioBtmNonAC -> "NonAc"
+                else -> "All"
+            }
+            m.coachType = coachType
+
+
+            val radioGroupSortByid = view.radioGroupSortBy.checkedRadioButtonId
+            val sortBy = when (radioGroupSortByid) {
+                R.id.radioLowPrice -> "Low Price"
+                R.id.radioHighPrice -> "HighPrice"
+                else -> "Low Price"
+            }
+            m.sortBy = sortBy
+            viewMode.onSort(m, isRetrunTriple)
+
+
+        }
 //
-//            val m = SearchFilter()
-//
-//            val checkedRadioButtonId = view.radioGroupJounyTime.checkedRadioButtonId
-//            val departingTime = when (checkedRadioButtonId) {
-//                R.id.radioJourneyTimeAll -> "Any"
-//                R.id.radioJourneyTimeAllMorning -> "Morning"
-//                R.id.radioJourneyTimeAllEvening -> "Evening"
-//                R.id.radioJourneyTimeAfterNoon -> "AfterNoon"
-//                R.id.radioJourneyTimeAllNight -> "Night"
-//
-//                else -> "Any"
-//            }
-//            m.departingTime = departingTime
-//
-//
-//            val id = view.radioGroupJounryType.checkedRadioButtonId
-//            val coachType = when (id) {
-//                R.id.radioBtmAll -> "All"
-//                R.id.radioBtmAC -> "Ac"
-//                R.id.radioBtmNonAC -> "NonAc"
-//                else -> "All"
-//            }
-//            m.coachType = coachType
-//
-//
-//            val radioGroupSortByid = view.radioGroupSortBy.checkedRadioButtonId
-//            val sortBy = when (radioGroupSortByid) {
-//                R.id.radioLowPrice -> "Low Price"
-//                R.id.radioHighPrice -> "HighPrice"
-//                else -> "Low Price"
-//            }
-//            m.sortBy = sortBy
-//            viewMode.onSort(m, isRetrunTriple)
-//
-//
-//        }
+
     }
 
     private fun hiddenAndShowFilter(view: View) {
 
-//        var size = 0
-//        if (!isRetrunTriple) {
-//            size = viewMode.singleTripTranportListMutableLiveData.value?.size ?: 0
-//        } else {
-//            size = viewMode.returnTripTransportListMutableLiveData.value?.size ?: 0
-//        }
-//        if (size > 0) {
+        var size = 0
+        if (!isRetrunTriple) {
+            size = viewMode.singleTripTranportListMutableLiveData.value?.size ?: 0
+        } else {
+            size = viewMode.returnTripTransportListMutableLiveData.value?.size ?: 0
+        }
+        if (size > 0) {
+
+            if (view.llAdvaceSerach.visibility == View.VISIBLE) {
+                view.llAdvaceSerach.visibility = View.GONE
+                layoutManager.isScrollEnabled = true
+                view.ivFilterUpDown.setImageResource(0)
+                view.ivFilterUpDown.setImageResource(R.drawable.arrow_down)
+            } else {
+                view.llAdvaceSerach.visibility = View.VISIBLE
+                layoutManager.isScrollEnabled = false
+                view.ivFilterUpDown.setImageResource(0)
+                view.ivFilterUpDown.setImageResource(R.drawable.arrow_up)
+            }
+        }
+
+
+
+//        val view: View = view.llCustomFilterHeader2//window.decorView.rootView
+//        val location = IntArray(2)
+//        view.getLocationOnScreen(location)
 //
-//            if (view.llAdvaceSerach.visibility == View.VISIBLE) {
-//                view.llAdvaceSerach.visibility = View.GONE
-//                layoutManager.isScrollEnabled = true
-//                view.ivFilterUpDown.setImageResource(0)
-//                view.ivFilterUpDown.setImageResource(R.drawable.arrow_down)
-//            } else {
-//                view.llAdvaceSerach.visibility = View.VISIBLE
-//                layoutManager.isScrollEnabled = false
-//                view.ivFilterUpDown.setImageResource(0)
-//                view.ivFilterUpDown.setImageResource(R.drawable.arrow_up)
-//            }
-//        }
-
-
-
-        val view: View = view.llCustomFilterHeader//window.decorView.rootView
-        val location = IntArray(2)
-        view.getLocationOnScreen(location)
-
-        p = Point()
-        p!!.x = location[0]
-        p!!.y = location[1]
-
-        if (p != null) showPopup(requireActivity(), p!!, view)
+//        p = Point()
+//        p!!.x = location[0]
+//        p!!.y = location[1]
+//
+//        if (p != null) showPopup(requireActivity(), p!!, view)
 
 
     }
@@ -248,6 +259,7 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
         popup!!.isOutsideTouchable = true
 
 
+
         val OFFSET_X = 30
         val OFFSET_Y = 30
 
@@ -256,6 +268,9 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
         popup!!.setBackgroundDrawable(null)
         popup!!.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y + OFFSET_Y)
 
+        val layoutParams  = layout.layoutParams as FrameLayout.LayoutParams
+        layoutParams.setMargins(30, -150, 30, 0)
+        layout.layoutParams = layoutParams
 
         var size = 0
         if (!isRetrunTriple) {
@@ -265,17 +280,17 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
         }
         if (size > 0) {
 
-//            if (layout.llAdvaceSerach.visibility == View.VISIBLE) {
-//                layout.llAdvaceSerach.visibility = View.GONE
-//                layoutManager.isScrollEnabled = true
+            if (layout.llAdvaceSerach.visibility == View.VISIBLE) {
+                layout.llAdvaceSerach.visibility = View.GONE
+                layoutManager.isScrollEnabled = true
 //                layout.ivFilterUpDown.setImageResource(0)
 //                layout.ivFilterUpDown.setImageResource(R.drawable.arrow_down)
-//            } else {
-//                layout.llAdvaceSerach.visibility = View.VISIBLE
-//                layoutManager.isScrollEnabled = false
+            } else {
+                layout.llAdvaceSerach.visibility = View.VISIBLE
+                layoutManager.isScrollEnabled = false
 //                layout.ivFilterUpDown.setImageResource(0)
 //                layout.ivFilterUpDown.setImageResource(R.drawable.arrow_up)
-//            }
+            }
         }
 
 
@@ -289,7 +304,7 @@ class TransportListFragment(val requestScheduledata: RequestScheduledata, val is
                 R.id.radioJourneyTimeAll -> "Any"
                 R.id.radioJourneyTimeAllMorning -> "Morning"
                 R.id.radioJourneyTimeAllEvening -> "Evening"
-                R.id.radioJourneyTimeAfterNoon -> "AfterNoon"
+               // R.id.radioJourneyTimeAfterNoon -> "AfterNoon"
                 R.id.radioJourneyTimeAllNight -> "Night"
 
                 else -> "Any"
